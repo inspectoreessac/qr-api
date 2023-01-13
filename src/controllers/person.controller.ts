37ae69b6fe739ef3e89933be3a2f7e9958723002
renato-av <renato.avela.17@gmail.com>
2023-01-13 11:01:32 -0500

    <feat>: adding remove handling

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import fs from 'fs-extra'
+import { In } from 'typeorm'
 import xlsx from 'xlsx'
 import AppDataSource from '../db'
 import { Person } from '../entities/person.entity'
@@ -78,6 +79,21 @@ export const remove = async (req: Request, res: Response): Promise<Response<Pers
   }
 }
 
+export const removeMultiple = async (req: Request, res: Response): Promise<Response<Person[]>> => {
+  const { ids } = req.body
+  const people = await peopleRepository.find({ where: { id: In(ids) } })
+
+  if (ids.length !== people.length) {
+    return res.status(400).json({ message: 'Hubo un error, un usuario no se encontró, intenta más tarde' })
+  }
+
+  try {
+    return res.json(await peopleRepository.remove(people))
+  } catch (error) {
+    return res.status(500).json({ message: getErrorMessage(error) })
+  }
+}
+
 const SHEET_NAMES = ['PERFIL A ACREDITAR', 'FECHA DE INDUCCIÓN', 'DOCUMENTO DE IDENTIDAD', 'NÚMERO DE DOCUMENTO DE IDENTIDAD', 'NOMBRES', 'APELLIDOS', 'EMPRESA', 'COD. CREDENCIAL', 'VIGENCIA CREDENCIAL', 'FOTOGRAFIA']
 
 export const importExcel = async (req: Request, res: Response): Promise<Response<Object>> => {
@@ -99,7 +115,9 @@ export const importExcel = async (req: Request, res: Response): Promise<Response
       return res.status(400).json({ message: 'El formato del archivo debe coincidir con el formato establecido' })
     }
 
-    const people = data.map((data: any) => {
+    const peopleInDatabase = await peopleRepository.find()
+
+    const peopleExcel = data.map((data: any) => {
       const obj: any = {}
       Object.keys(data).forEach(key => {
         obj[key.toUpperCase()] = data[key]
@@ -107,12 +125,11 @@ export const importExcel = async (req: Request, res: Response): Promise<Response
       const aux: string = obj.FOTOGRAFIA
       const imageId = aux.split('?id=')[1]
       const profileImage = `https://drive.google.com/uc?export=view&id=${imageId}`
-
       return {
         profile: obj['PERFIL A ACREDITAR'],
         inductionDate: obj['FECHA DE INDUCCIÓN'],
         docType: obj['DOCUMENTO DE IDENTIDAD'],
-        docNum: obj['NÚMERO DE DOCUMENTO DE IDENTIDAD'],
+        docNum: String(obj['NÚMERO DE DOCUMENTO DE IDENTIDAD']),
         name: obj.NOMBRES,
         lastName: obj.APELLIDOS,
         company: obj.EMPRESA,
@@ -121,6 +138,13 @@ export const importExcel = async (req: Request, res: Response): Promise<Response
         profileImage
       }
     })
+
+    const people = peopleExcel.filter((personExcel, index) => {
+      return peopleExcel.findIndex(person => person.docNum === personExcel.docNum) === index
+    })
+      .filter(personToSave => {
+        return peopleInDatabase.find(person => person.docNum === personToSave.docNum) === undefined
+      })
 
     await fs.remove(filename)
     return res.status(200).json(await peopleRepository.save(people))
--- src/routes/person.routes.ts
+++ src/routes/person.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { create, getAll, getByDni, getById, importExcel, remove, update } from '../controllers/person.controller'
+import { create, getAll, getByDni, getById, importExcel, remove, removeMultiple, update } from '../controllers/person.controller'
 import upload from '../helpers/file.helper'
 import { requireAuth } from '../middlewares/require-auth'
 
@@ -11,6 +11,7 @@ router.get('/people', requireAuth, getAll)
 router.get('/people/:id', getById)
 router.post('/people', requireAuth, create)
 router.put('/people/:id', requireAuth, update)
+router.patch('/people/remove-multiple', requireAuth, removeMultiple)
 router.delete('/people/:id', requireAuth, remove)
 
 router.post('/people/import-excel', requireAuth, upload.single('excel-file'), importExcel)
